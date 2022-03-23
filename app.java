6f2696d3140daacd24c0bf0d3c00c723547d2ad8
yassir28 <yassirmakloul@gmail.com>
2022-03-23 14:15:10 +0100

    ye

--- app.java
+++ app.java
@@ -3,7 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("yesssss!"); 
+        System.out.println("yahoooo!"); 
 
     }
 }
